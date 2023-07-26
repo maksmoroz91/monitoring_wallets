69de12583a0f9268101e320474461d3c6618cc74
maksmoroz91 <dlkontakta7@gmail.com>
2023-07-26 16:25:29 +0300

    added shema.prisma

---
+++ prisma/migrations/20230726131444_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE `Contract` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `address` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Contract_address_key`(`address`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `amount` VARCHAR(191) NOT NULL,
+    `fromAddress` VARCHAR(191) NOT NULL,
+    `decimals` INTEGER NOT NULL,
+    `tokenName` VARCHAR(191) NOT NULL,
+    `toAddress` VARCHAR(191) NOT NULL,
+    `block` INTEGER NOT NULL,
+    `confirmed` BOOLEAN NOT NULL,
+    `hash` VARCHAR(191) NOT NULL,
+    `timestamp` BIGINT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Wallet` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `address` VARCHAR(191) NOT NULL,
+    `tokenCount` INTEGER NOT NULL,
+    `countAllTokens` JSON NOT NULL,
+    `totalBalanceUSD` DOUBLE NOT NULL,
+
+    UNIQUE INDEX `Wallet_address_key`(`address`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
